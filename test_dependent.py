56042fa7970eb2440ac2745ee3dcc7a3144d9246
khant2905 <6805140019@siam.edu>
2026-09-14 00:49:55 +0700

    Add files via upload

---
+++ test_dependent.py
@@ -0,0 +1,14 @@
+from bank import BankAccount
+
+shared_account = BankAccount(100)
+
+def test_a_deposit():
+    shared_account.deposit(50)
+    assert shared_account.balance ==150
+
+def test_b_withdraw():
+        shared_account.withdraw(50)
+        assert shared_account.balance == 150
+
+
+
